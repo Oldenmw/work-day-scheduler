42324bb78691fe890c1d14970906d2c74b6ff6bc
Matt Oldenburg <theoldenmw@gmail.com>
2021-08-31 16:09:24 -0400

    added task editing

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -1,39 +1,79 @@
 var dateEl = document.querySelector("#currentDay");
 var currentHour = new Date().getHours();
+var tasks = {};
 
 var fillCurrentDate = function() {
     var currentDate = moment().format('dddd, MMMM Do');
     dateEl.textContent = currentDate;
 }
 
-$(".task-text").on("click", "p", function() {
-    // get current text of p element
-    var text = $(this)
-      .text()
-      .trim();
-  
-    // replace p element with a new textarea
+$(".task-body").on("click", function() {
+    var text = $(this).children('p')
+        .text()
+        .trim();
+    var pId = $(this).children('p').attr('id');
+    console.log(pId);
+
     var textInput = $("<textarea>")
-      .addClass("form-control")
-      .val(text);
-    $(this).replaceWith(textInput);
+        .addClass("col-10 task-input")
+        .attr('id', pId)
+        .val(text);
+    $(this).children('p').replaceWith(textInput);
   
-    // auto focus new element
     textInput.trigger("focus");
+});
+
+$(".task-body").focusout( function() {
+    var text = $(this).children("textarea")
+        .val();
+    // update task in array and re-save to localstorage
+    // tasks[status][index].text = text;
+    // saveTasks();
+  
+    var taskP = $("<p>")
+        .text(text);
+    $(this).children("textarea").replaceWith(taskP);
   });
 
 var timeBlocks = function(hour) {
     var divEl = document.querySelector("#hour-" + hour + " .col-10");
     
     if ((currentHour - hour) > 9) {
-        divEl.setAttribute("class", "col-10 past");
+        divEl.setAttribute("class", "col-10 task-body past");
     }
     else if ((currentHour - hour) === 9) {
-        divEl.setAttribute("class", "col-10 present");
+        divEl.setAttribute("class", "col-10 task-body present");
     }
     else if ((currentHour - hour) < 9) {
-        divEl.setAttribute("class", "col-10 future");
+        divEl.setAttribute("class", "col-10 task-body future");
     }
+};
+
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  
+    // if nothing in localStorage, create a new object to track all task status arrays
+    if (!tasks) {
+      tasks = {
+        hour0: [],
+        hour1: [],
+        hour2: [],
+        hour3: [],
+        hour4: [],
+        hour5: [],
+        hour6: [],
+        hour7: [],
+        hour8: []
+      };
+    }
+  
+    // loop over object properties
+    $.each(tasks, function(list, arr) {
+      // then loop over sub-array
+      arr.forEach(function(task) {
+        var pEl = document.querySelector("#")
+      });
+    });
 };
 
 fillCurrentDate();
@@ -44,4 +84,7 @@ var fillTaskBlocks = function() {
     };
 };
 fillTaskBlocks();
-console.log(currentHour);
+
+setInterval(function() {
+    fillTaskBlocks();
+}, (1000 * 60) * 10);
